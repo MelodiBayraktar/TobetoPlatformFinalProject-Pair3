e45e44142fd7ddfb9719738166b1829be6080468
atacanguzelkaya <atacangzlky@gmail.com>
2024-01-16 20:09:52 +0300

    Survey, User, UserOperationClaim added.

---
+++ TobetoPlatformFinalProject-Pair3/Entities/Concretes/Survey.cs
@@ -0,0 +1,12 @@
+﻿using Core.Entities.Concretes;
+
+namespace Entities.Concretes;
+
+public class Survey : Entity<Guid>
+{
+    public Guid? StudentId { get; set; }
+    public string? Title { get; set; }
+    public string? Url { get; set; }
+
+    public Student? Student { get; set; }
+}
---
+++ TobetoPlatformFinalProject-Pair3/Entities/Concretes/UserOperationClaim.cs
@@ -0,0 +1,13 @@
+﻿using Core.Entities.Abstracts;
+using Core.Entities.Concretes;
+
+namespace Entities.Concretes;
+public class UserOperationClaim : Entity<Guid>, IUserOperationClaim
+{
+    public Guid? UserId { get; set; }
+    public Guid? OperationClaimId { get; set; }
+
+
+    public User? User { get; set; }
+    public OperationClaim? OperationClaim { get; set; }
+}
