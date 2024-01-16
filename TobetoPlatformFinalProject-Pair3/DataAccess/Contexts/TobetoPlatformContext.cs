6ea3b2eb8d599e892f9ce9c7038d8433b9fe19af
atacanguzelkaya <atacangzlky@gmail.com>
2024-01-16 20:56:08 +0300

    TobetoPlatformContext and DataAccessServiceRegistration added in DataAccess layer.

---
+++ TobetoPlatformFinalProject-Pair3/DataAccess/Contexts/TobetoPlatformContext.cs
@@ -0,0 +1,60 @@
+﻿using Entities.Concretes;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Contexts;
+
+public class TobetoPlatformContext : DbContext
+{
+    protected IConfiguration Configuration { get; set; }
+    public DbSet<Ability> Abilities { get; set; }
+    public DbSet<AnnouncementsNewsCategory> AnnouncementsNewsCategorys { get; set; }
+    public DbSet<Application> Applications { get; set; }
+    public DbSet<Announcement> Announcements { get; set; }
+    public DbSet<AsyncContent> AsyncContents { get; set; }
+    public DbSet<AsyncCourse> AsyncCourses { get; set; }
+    public DbSet<News> News { get; set; }
+    public DbSet<AsyncLessonsOfContent> AsyncLessonsOfContents { get; set; }
+    public DbSet<AsyncLessonsDetail> AsyncLessonsDetails { get; set; }
+    public DbSet<Certificate> Certificates { get; set; }
+    public DbSet<ContactUs> ContactUs { get; set; }
+    public DbSet<Course> Courses { get; set; }
+    public DbSet<CourseCategory> CourseCategories { get; set; }
+    public DbSet<CourseDetail> CourseDetails { get; set; }
+    public DbSet<CourseExam> CourseExams { get; set; }
+    public DbSet<Education> Educations { get; set; }
+    public DbSet<Experience> Experiences { get; set; }
+    public DbSet<ForeignLanguage> ForeignLanguages { get; set; }
+    public DbSet<Instructor> Instructors { get; set; }
+    public DbSet<LiveContent> LiveContents { get; set; }
+    public DbSet<LiveCourse> LiveCourses { get; set; }
+    public DbSet<PasswordReset> PasswordReset { get; set; }
+    public DbSet<PersonalInfo> PersonalInfos { get; set; }
+    public DbSet<Project> Projects { get; set; }
+    public DbSet<Session> Sessions { get; set; }
+    public DbSet<Settings> Settings { get; set; }
+    public DbSet<SocialAccount> SocialAccounts { get; set; }
+    public DbSet<Student> Students { get; set; }
+    public DbSet<Survey> Surveys { get; set; }
+    public DbSet<User> Users { get; set; }
+    public DbSet<OperationClaim> OperationClaims { get; set; }
+    public DbSet<UserOperationClaim> UserOperationClaims { get; set; }
+
+    public TobetoPlatformContext(DbContextOptions dbContextOptions, IConfiguration configuration) : base(dbContextOptions)
+    {
+        Configuration = configuration; 
+        Database.EnsureCreated();
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
+}
+
---
+++ TobetoPlatformFinalProject-Pair3/DataAccess/DataAccessServiceRegistration.cs
@@ -0,0 +1,56 @@
+﻿using DataAccess.Abstracts;
+using DataAccess.Concretes;
+using DataAccess.Concretes.EntityFramework;
+using DataAccess.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess;
+
+public static class DataAccessServiceRegistration
+{
+    public static IServiceCollection AddDataAccessServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<TobetoPlatformContext>(options => options.UseSqlServer(configuration.GetConnectionString("TobetoPlatformDb")));
+        services.AddScoped<IAbilityDal, EfAbilityDal>();
+        services.AddScoped<IAnnouncementDal, EfAnnouncementDal>();
+        services.AddScoped<IAnnouncementsNewsCategoryDal, EfAnnouncementsNewsCategoryDal>();
+        services.AddScoped<IApplicationDal, EfApplicationDal>();
+        services.AddScoped<IAsyncContentDal, EfAsyncContentDal>();
+        services.AddScoped<IAsyncCourseDal, EfAsyncCourseDal>();
+        services.AddScoped<IAsyncLessonsOfContentDal, EfAsyncLessonsOfContentDal>();
+        services.AddScoped<IAsyncLessonsDetailDal, EfAsyncLessonsDetailDal>();
+        services.AddScoped<ICertificateDal, EfCertificateDal>();
+        services.AddScoped<IContactUsDal, EfContactUsDal>();
+        services.AddScoped<ICourseDal, EfCourseDal>();
+        services.AddScoped<ICourseCategoryDal, EfCourseCategoryDal>();
+        services.AddScoped<ICourseDetailDal, EfCourseDetailDal>();
+        services.AddScoped<ICourseExamDal, EfCourseExamDal>();
+        services.AddScoped<IEducationDal, EfEducationDal>();
+        services.AddScoped<IExperienceDal, EfExperienceDal>();
+        services.AddScoped<IForeignLanguageDal, EfForeignLanguageDal>();
+        services.AddScoped<IInstructorDal, EfInstructorDal>();
+        services.AddScoped<ILiveContentDal, EfLiveContentDal>();
+        services.AddScoped<ILiveCourseDal, EfLiveCourseDal>();
+        services.AddScoped<INewsDal, EfNewsDal>();
+        services.AddScoped<IPasswordResetDal, EfPasswordResetDal>();
+        services.AddScoped<IPersonalInfoDal, EfPersonalInfoDal>();
+        services.AddScoped<IProjectDal, EfProjectDal>();
+        services.AddScoped<ISessionDal, EfSessionDal>();
+        services.AddScoped<ISettingsDal, EfSettingsDal>();
+        services.AddScoped<ISocialAccountDal, EfSocialAccountDal>();
+        services.AddScoped<IStudentDal, EfStudentDal>();
+        services.AddScoped<ISurveyDal, EfSurveyDal>();
+        services.AddScoped<IUserDal, EfUserDal>();
+        services.AddScoped<IUserOperationClaimDal, EfUserOperationClaimDal>();
+        services.AddScoped<IOperationClaimDal, EfOperationClaimDal>();
+
+        return services;
+    }
+}
