fa3cfc24ee4504f7c1fa4f774ad60c62927e3161
Gofrettyy <naz_1216@outlook.com>
2024-01-16 19:26:04 +0300

    AsyncLessonDetail AsyncLessonsOfContent ve Certificate added.

---
+++ TobetoPlatformFinalProject-Pair3/Entities/Concretes/AsyncLessonsDetail.cs
@@ -0,0 +1,16 @@
+using Core.Entities.Concretes;
+
+namespace Entities.Concretes;
+
+public class AsyncLessonsDetail : Entity<Guid>
+{
+    public Guid? AsyncLessonId { get; set; }
+    public bool? IsLiked { get; set; }
+    public string? Category { get; set; }
+    public string? Language { get; set; }
+    public string? SubType { get; set; }
+    public string? Description { get; set; }
+    public string? ProducingCompany { get; set; }
+
+    public AsyncLessonsOfContent? AsyncLessonsOfContent { get; set; }
+}
---
+++ TobetoPlatformFinalProject-Pair3/Entities/Concretes/Certificate.cs
@@ -0,0 +1,14 @@
+﻿using Core.Entities.Concretes;
+
+namespace Entities.Concretes;
+
+public class Certificate : Entity<Guid>
+{
+    public Guid? UserId { get; set;}
+    public string? FilePath { get; set; }
+    public string? FileType { get; set; }
+    public DateTime? UploadDate { get; set; }
+
+    public User? User { get; set; }
+}
+
