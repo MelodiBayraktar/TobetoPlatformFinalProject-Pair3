ca2112728ad784f38bba10de1b3deb19f3d77d43
serkanozak <av.serkanozak@gmail.com>
2024-01-16 20:02:53 +0300

    Project and Session added.

---
+++ TobetoPlatformFinalProject-Pair3/Entities/Concretes/Project.cs
@@ -0,0 +1,12 @@
+﻿using Core.Entities.Concretes;
+
+namespace Entities.Concretes;
+
+public class Project : Entity<Guid>
+{
+    public string? Name { get; set; }
+
+    public List<Application>? Applications { get; set; }
+    public List<News>? News { get; set; }
+    public List<Announcement>? Announcements { get; set; } 
+}
---
+++ TobetoPlatformFinalProject-Pair3/Entities/Concretes/Session.cs
@@ -0,0 +1,18 @@
+﻿using Core.Entities.Concretes;
+
+namespace Entities.Concretes;
+
+public class Session : Entity<Guid>
+{
+    public Guid? InstructorId { get; set; }
+    public Guid? LiveContentId { get; set; }
+    public string? Name { get; set; }    
+    public string? ImageUrl { get; set; }
+    public DateTime? StartDate { get; set; }
+    public DateTime? EndDate { get; set; }
+    public string? RecordUrl { get; set; }
+    public string? SessionLinkUrl { get; set; }
+
+    public LiveContent? LiveContent { get; set; }
+    public Instructor? Instructor { get; set; }
+}
