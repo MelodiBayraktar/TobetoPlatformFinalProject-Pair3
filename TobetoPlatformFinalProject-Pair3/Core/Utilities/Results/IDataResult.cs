88c4bdb96b0cbc7546daefa3b4a910ce8260e1e2
mrtcrpr <mrt.crpr@gmail.com>
2024-01-16 21:47:24 +0300

    Results added.

---
+++ TobetoPlatformFinalProject-Pair3/Core/Utilities/Results/IDataResult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities.Results
+{
+    public interface IDataResult<T> : IResult
+    {
+
+        //bir interface başka bir interface'i implemente ederse, sanki implemente edilen interface içerisindekileri de kapsar gibi olur. Base classlar gibi düşün.
+        T Data { get; }
+    }
+}
