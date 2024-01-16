e286ac47b42c5878fe16eee4ab2075c37137eab0
MelodiBayraktar <melodi_bayraktar@hotmail.com>
2024-01-16 19:07:21 +0300

    Application, AsyncContent,AsyncCourse entities added

---
+++ TobetoPlatformFinalProject-Pair3/Entities/Concretes/AsyncContent.cs
@@ -0,0 +1,11 @@
+﻿using Core.Entities.Concretes;
+
+namespace Entities.Concretes;
+
+public class AsyncContent : Entity<Guid>
+{
+    public Guid? AsyncCourseId { get; set; }
+    public string? Name { get; set; }
+    public AsyncCourse? AsyncCourse { get; set; }
+    public List<AsyncLessonsOfContent>? AsyncLessonsOfContents { get; set; }
+}
