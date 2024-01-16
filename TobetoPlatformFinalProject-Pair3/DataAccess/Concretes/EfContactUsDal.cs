1f7425121d3f43f76014e19f2c541cf58e5068ea
serkanozak <av.serkanozak@gmail.com>
2024-01-16 20:29:33 +0300

    Concretes added in DataAccess layer.

---
+++ TobetoPlatformFinalProject-Pair3/DataAccess/Concretes/EfContactUsDal.cs
@@ -0,0 +1,14 @@
+﻿using Core.DataAccess.Repositories;
+using DataAccess.Abstracts;
+using DataAccess.Contexts;
+using Entities.Concretes;
+
+namespace DataAccess.Concretes;
+
+public class EfContactUsDal : EfRepositoryBase<ContactUs, Guid, TobetoPlatformContext>, IContactUsDal
+{
+    public EfContactUsDal(TobetoPlatformContext context) : base(context)
+    {
+    }
+}
+
