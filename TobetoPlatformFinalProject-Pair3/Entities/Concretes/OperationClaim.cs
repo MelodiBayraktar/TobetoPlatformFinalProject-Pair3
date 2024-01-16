7ce0e1cf1cf11598456df8e89c79fcebb40cec32
mrtcrpr <mrt.crpr@gmail.com>
2024-01-16 19:59:31 +0300

    OperationClaim, PasswordReset and PersonalInfo added.

---
+++ TobetoPlatformFinalProject-Pair3/Entities/Concretes/OperationClaim.cs
@@ -0,0 +1,10 @@
+﻿using Core.Entities.Abstracts;
+using Core.Entities.Concretes;
+
+namespace Entities.Concretes;
+public class OperationClaim : Entity<Guid>, IOperationClaim
+{
+    public string? Name { get; set; }
+
+    public List<UserOperationClaim>? Users { get; set; }
+}
---
+++ TobetoPlatformFinalProject-Pair3/Entities/Concretes/PasswordReset.cs
@@ -0,0 +1,12 @@
+﻿using Core.Entities.Concretes;
+
+namespace Entities.Concretes;
+
+public class PasswordReset : Entity<Guid>
+{
+    public Guid? UserId { get; set; }
+    public string? VerificationCode { get; set; }
+    public bool? IsUsed { get; set; }
+
+    public User? User { get; set; }
+}
