77249ab1e4c50e7efb13699db43789f3b5f85830
Gofrettyy <naz_1216@outlook.com>
2024-01-16 22:16:24 +0300

    Constants added in Business layer.

---
+++ TobetoPlatformFinalProject-Pair3/Business/Constants/Messages/CourseMessages.cs
@@ -0,0 +1,10 @@
+namespace Business.Constants;
+
+public class CourseMessages
+{
+    public static string AddedCourse = "Course is added";
+    public static string DeletedCourse = "Course is deleted";
+    public static string UpdatedCourse = "Course is updated";
+    public static string ListedCourse = "Course is listed";
+    
+}
---
+++ TobetoPlatformFinalProject-Pair3/Business/Constants/Messages/UserMessages.cs
@@ -0,0 +1,13 @@
+namespace Business.Constants;
+
+public class UserMessages
+{
+    public static string AuthorizationDenied="Authorization is Denied";
+    public static string UserNotBeExist = "User not be exist";
+    public static string UserAlreadyExist = "User already exist";
+    
+    public static string MustContainAtMinTwoChar = "Must contain at minimum 2 characters";
+    public static string MustContainAtMaxTenChar = "Must contain a maximum of 10 characters";
+    public static string PleaseEnterAStrongerPassword = "Please enter a stronger password";
+    public static string PleaseEnterAValidNationalyIdNumber = "Please enter a valid nationality id number";
+}
