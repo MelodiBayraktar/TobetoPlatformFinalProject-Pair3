a7d52a775196799986cbb0d1bf35fd67903c9621
serkanozak <av.serkanozak@gmail.com>
2024-01-16 20:32:27 +0300

    Concretes added in DataAccess layer.

---
+++ TobetoPlatformFinalProject-Pair3/DataAccess/Concretes/EfSocialAccountDal.cs
@@ -0,0 +1,14 @@
+﻿using Core.DataAccess.Repositories;
+using DataAccess.Abstracts;
+using DataAccess.Contexts;
+using Entities.Concretes;
+
+namespace DataAccess.Concretes;
+
+public class EfSocialAccountDal : EfRepositoryBase<SocialAccount, Guid, TobetoPlatformContext>, ISocialAccountDal
+{
+    public EfSocialAccountDal(TobetoPlatformContext context) : base(context)
+    {
+    }
+}
+
---
+++ TobetoPlatformFinalProject-Pair3/DataAccess/Concretes/EfUserDal.cs
@@ -0,0 +1,37 @@
+﻿using Core.DataAccess.Repositories;
+using Core.Entities.Abstracts;
+using DataAccess.Abstracts;
+using DataAccess.Contexts;
+using Entities.Concretes;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Concretes;
+
+public class EfUserDal : EfRepositoryBase<User, Guid, TobetoPlatformContext>, IUserDal
+{
+    TobetoPlatformContext _context;
+    public EfUserDal(TobetoPlatformContext context) : base(context)
+    {
+        _context = context;
+    }
+
+    public List<IOperationClaim> GetClaims(IUser user)
+    {
+        using (_context)
+        {
+            var result = from operationClaim in _context.OperationClaims
+                         join userOperationClaim in _context.UserOperationClaims
+                             on operationClaim.Id equals userOperationClaim.OperationClaimId
+                         //where userOperationClaim.UserId == user.Id burayı mecburen boxing yaptım.
+                         where userOperationClaim.UserId == ((User)user).Id
+                         select (IOperationClaim)(new OperationClaim
+                         {
+                             Id = operationClaim.Id,
+                             Name = operationClaim.Name
+                         });
+
+            return result.ToList();
+        }
+    }
+}
+
