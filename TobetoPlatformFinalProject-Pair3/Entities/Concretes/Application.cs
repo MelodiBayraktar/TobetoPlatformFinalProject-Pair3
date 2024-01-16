02cd2c81411eeed9439a9c28a475d9b909294673
MelodiBayraktar <melodi_bayraktar@hotmail.com>
2024-01-16 19:07:21 +0300

    Application, AsyncContent,AsyncCourse entities added

---
+++ TobetoPlatformFinalProject-Pair3/Entities/Concretes/Application.cs
@@ -0,0 +1,15 @@
+﻿using Core.Entities.Concretes;
+
+namespace Entities.Concretes;
+
+public class Application : Entity<Guid>
+{
+    public Guid? ProjectId { get; set; }
+    
+    public string? Description { get; set; }
+    public string? ApplicationForStatus { get; set; }
+    public string? DocumentUploadForStatus { get; set; }
+    public bool? ApplicationStatus { get; set; }
+
+    public Project? Project { get; set; }
+}
---
+++ TobetoPlatformFinalProject-Pair3/Entities/Concretes/AsyncCourse.cs
@@ -0,0 +1,15 @@
+﻿using Core.Entities.Concretes;
+
+namespace Entities.Concretes;
+
+public class AsyncCourse : Entity<Guid>
+{
+    public Guid? CourseId { get; set; }
+    public Guid? CourseDetailId { get; set; }
+    public DateTime? EstimatedTime { get; set; }
+    public string? ProducingCompany { get; set; }
+
+    public Course? Course { get; set; }
+    public List<AsyncContent>? AsyncContents { get; set; }
+    public CourseDetail? CourseDetail { get; set; }
+}
