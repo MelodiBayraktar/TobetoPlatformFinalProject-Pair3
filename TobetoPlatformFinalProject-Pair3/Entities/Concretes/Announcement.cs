41472c4d6ae8edcd9d96ed1ceb808fcd38f8a81c
MelodiBayraktar <melodi_bayraktar@hotmail.com>
2024-01-16 19:04:57 +0300

    Ability, Announcement, AnnouncementsNewsCategory entities added

---
+++ TobetoPlatformFinalProject-Pair3/Entities/Concretes/Announcement.cs
@@ -0,0 +1,13 @@
+﻿using Core.Entities.Concretes;
+
+namespace Entities.Concretes;
+
+public class Announcement : Entity<Guid>
+{
+    public Guid? AnnouncementsNewsCategoryId { get; set; }
+    public Guid? ProjectId { get; set; }
+    public string? Title { get; set; }
+    public string? AnnouncementContent { get; set; }
+    public Project? Project { get; set; }
+    public AnnouncementsNewsCategory? AnnouncementsNewsCategory { get; set;}
+}
---
+++ TobetoPlatformFinalProject-Pair3/Entities/Concretes/AnnouncementsNewsCategory.cs
@@ -0,0 +1,11 @@
+﻿using Core.Entities.Concretes;
+
+namespace Entities.Concretes;
+
+public class AnnouncementsNewsCategory : Entity<Guid>
+{
+    public string? Name { get; set; }
+    public List<Announcement>? Announcements { get; set; }
+    public List<News>? News { get; set; }
+}
+
